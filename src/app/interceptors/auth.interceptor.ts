26031081232dfbd86f84143edbe1f71d319e2aee
raresd212 <draghici.rares212@gmail.com>
2022-07-13 16:30:32 +0300

    with credentials

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -13,6 +13,9 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    request = request.clone({
+      withCredentials: true
+    });
     return next.handle(request);
   }
 }
